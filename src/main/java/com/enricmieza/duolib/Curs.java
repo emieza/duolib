5233cee4c5f8bd96693458eedc909e5c4e524b06
Enricus <emieza@xtec.cat>
2020-11-08 22:30:48 +0100

    Classes derivades per tipus d'exercicis

--- src/main/java/com/enricmieza/duolib/Curs.java
+++ src/main/java/com/enricmieza/duolib/Curs.java
@@ -5,7 +5,14 @@ import java.util.ArrayList;
 public class Curs {
 
     public class Exercici {
+        public static final int TRADUCCIO_OBERTA = 1;
+        public static final int TRADUCCIO_REORDENA_PARAULES = 2;
+        public static final int LISTENING_REORDENA = 3;
+        public static final int LISTENING_OBERT = 4;
+        public static final int APARELLA_PARAULES = 5;
+
         public String titol;
+        public int tipus;
         public String instruccions;
         public String fraseOrigen;
         public String fraseDesti;
@@ -21,6 +28,7 @@ public class Curs {
 
             Exercici e1 = new Exercici();
             e1.titol = "Traduccio1";
+            e1.tipus = Exercici.TRADUCCIO_OBERTA;
             e1.instruccions = "Tradueix la frase";
             e1.fraseOrigen = "Un bon día al matí";
             e1.fraseDesti = "A nice day in the morning";
@@ -28,6 +36,7 @@ public class Curs {
 
             Exercici e2 = new Exercici();
             e2.titol = "Traduccio2";
+            e2.tipus = Exercici.TRADUCCIO_REORDENA_PARAULES;
             e2.instruccions = "Tradueix la frase";
             e2.fraseOrigen = "Dábale arroz a la zorra el abad";
             e2.fraseDesti = "The abbot was giving rice to the fox";
